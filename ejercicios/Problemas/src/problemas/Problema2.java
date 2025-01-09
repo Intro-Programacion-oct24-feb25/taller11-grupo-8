c49a6d3f4e91a4fceadb37bf1de33ed0c670b1fb
An-dr3a <criolloandrea20@hotmail.com>
2025-01-09 12:23:44 -0500

    Problema2 y 3

--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,7 +18,67 @@ public class Problema2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        Scanner entrada = new Scanner(System.in);
+
+        int opcion;
+        System.out.println("Para calcular el area de un cuadrado ingrese 1\n"
+                + "Para calcular el area de un triangulo ingrese 2\nPara "
+                + "calcular el area de un rectantangulo ingrese el 3 ");
+        System.out.print("Ingrese un numero del 1 al 3 para ingresar en "
+                + "la operacion: \n");
+        opcion = entrada.nextInt();
+        switch (opcion) {
+            case 1:
+                obtenerAreaCuadrado();
+                break;
+            case 2:
+                obtenerAreaTriangulo();
+                break;
+            case 3:
+                obtenerAreaRectangulo();
+                break;
+
+        }
     }
-    
+
+    public static void obtenerAreaCuadrado() {
+        Scanner entrada = new Scanner(System.in);
+        double lado;
+        double lado2;
+
+        System.out.println("Ingrese la medida de uno de los lados del cuadrado"
+                + ":");
+        lado = entrada.nextDouble();
+        lado2 = lado * lado;
+        System.out.printf("El area de el cuadrado es:%.0f\n", lado2);
+    }
+
+    public static void obtenerAreaTriangulo() {
+        Scanner entrada = new Scanner(System.in);
+        double altura;
+        double base;
+        double area;
+
+        System.out.println("Ingrese la base del triangulo:");
+        base = entrada.nextDouble();
+        System.out.println("Ingrese la altura del triangulo:");
+        altura = entrada.nextDouble();
+        area = (base * altura) / 2;
+        System.out.printf("El area de el triangulo es:%.1f\n", area);
+    }
+
+    public static void obtenerAreaRectangulo() {
+        Scanner entrada = new Scanner(System.in);
+        double base;
+        double altura;
+        double area;
+
+        System.out.println("Ingrese la base del rectangulo:");
+        base = entrada.nextDouble();
+        System.out.println("Ingrese la altura del rectangulo:");
+        altura = entrada.nextDouble();
+        area = base * altura;
+        System.out.printf("El area de el rectangulo es:%.0f\n", area);
+    }
+
 }
--- ejercicios/Problemas/src/problemas/Problema3.java
+++ ejercicios/Problemas/src/problemas/Problema3.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package problemas;
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +16,39 @@ public class Problema3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         // TODO code application logic here
+        double[] notas = new double[4];
+        String cualitativo;
+        for (int i = 0; i <= 3; i++){
+            System.out.println("Ingrese la nota");
+            notas[i] = entrada.nextDouble();           
+        }
+        
+        cualitativo = funcion1(notas[0], notas[1], notas[2], notas[3]);
+        double promedio = (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
+        System.out.println("El promedio de las notas: " + notas[0] + ", " + notas[1] + ", " + notas[2] +
+                "; " + notas[3] + " es: " + cualitativo) ;
+    }
+    
+    public static String funcion1(double a, double b, double c, double d) {
+        // 
+        int promedio = 0;
+        String mensaje = "";
+        promedio = (int) ((a + b +c + d )/4);
+        if (promedio <= 5) {
+            mensaje = "Regular";
+        } else {
+            if (promedio > 5 && promedio <=8) {
+                mensaje = "Muy bueno";
+            } else {
+                if (promedio > 9 && promedio <= 10){
+                    mensaje = "Sobre Saliente";
+                }
+            }
+        }
+        return mensaje;
+        
     }
     
 }
